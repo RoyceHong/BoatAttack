4bc66d3f6235b80f2fbf13864abef57616b10b6f
RoyceHong <rhchen73@utexas.edu>
2021-03-16 19:15:56 -0500

    Add delay to survey button

---
+++ Assets/Scripts/UI/DelayButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayButton : MonoBehaviour
+{
+    GameObject survey_button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        survey_button = GameObject.Find("Survey_Button");
+        survey_button.SetActive(false);
+        Invoke("Delay", 15);
+        
+    }
+
+    public void Delay()
+    {
+        Debug.Log("Activate Survey Button : " + Time.time);
+        survey_button.SetActive(true);
+    }
+}
